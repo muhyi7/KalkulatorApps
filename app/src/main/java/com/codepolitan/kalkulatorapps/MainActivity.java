21a6a03fc00dfd4a47a520c264c388e8ea8ef25b
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-07-10 15:31:31 +0700

    finish

--- app/src/main/java/com/codepolitan/kalkulatorapps/MainActivity.java
+++ app/src/main/java/com/codepolitan/kalkulatorapps/MainActivity.java
@@ -1,12 +1,12 @@
 package com.codepolitan.kalkulatorapps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
